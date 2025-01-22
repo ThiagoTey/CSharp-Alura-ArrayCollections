62656f24a55d56959b74ed5174c208d21e326e98
Thiago <teyextreme@outlook.com>
2025-01-22 12:41:56 -0300

    Funcao de aumentar capacidade de um array de objeto de classe

--- bytebank_ATENDIMENTO/Program.cs
+++ bytebank_ATENDIMENTO/Program.cs
@@ -1,6 +1,9 @@
-﻿Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
+﻿using bytebank.Modelos.Conta;
+using bytebank_ATENDIMENTO.bytebank.Util;
 
-Array amostra = Array.CreateInstance(typeof(double), 5);
+Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
+
+Array amostra = new double[5];
 amostra.SetValue(1.1, 0);
 amostra.SetValue(5, 1);
 amostra.SetValue(7.2, 2);
@@ -10,20 +13,40 @@ amostra.SetValue(6.9, 4);
 
 void TestsMediana(Array array)
 {
-    if(array == null || array.Length==0)
+    if (array == null || array.Length == 0)
     {
         Console.WriteLine("Array para cálculo está vazio ou nulo");
         return;
     }
 
-    double[] numerosOrdenados = (double [])array.Clone();
+    double[] numerosOrdenados = (double[])array.Clone();
     Array.Sort(numerosOrdenados);
 
     int tamanho = numerosOrdenados.Length;
     int meio = tamanho / 2;
-    double mediana = (tamanho %2 != 0) ? numerosOrdenados[meio] : (numerosOrdenados[meio]  + numerosOrdenados[meio - 1]) / 2;
+    double mediana = (tamanho % 2 != 0) ? numerosOrdenados[meio] : (numerosOrdenados[meio] + numerosOrdenados[meio - 1]) / 2;
 
     Console.WriteLine($"Mediana = {mediana}");
 }
 
-TestsMediana(amostra);
+//TestsMediana(amostra);
+
+double[] arrayDouble = { 1.1, 5.5, 7.8, 7.7, 10 };
+
+void CalculaMedeia(double[] array)
+{
+    array.Average();
+}
+
+void TestaArrayDeContasCorrentes()
+{
+    ListaDeContasCorrentes listaDeContas = new();
+    listaDeContas.Adicionar(new ContaCorrente(874, "5679787-A"));
+    listaDeContas.Adicionar(new ContaCorrente(874, "4456668-B"));
+    listaDeContas.Adicionar(new ContaCorrente(874, "7781438-C"));
+    listaDeContas.Adicionar(new ContaCorrente(874, "7781438-D"));
+    listaDeContas.Adicionar(new ContaCorrente(874, "7781438-E"));
+    listaDeContas.Adicionar(new ContaCorrente(874, "7781438-E"));
+}
+
+TestaArrayDeContasCorrentes();
---
+++ bytebank_ATENDIMENTO/bytebank.Util/ListaDeContasCorrentes.cs
@@ -0,0 +1,39 @@
+﻿using bytebank.Modelos.Conta;
+
+namespace bytebank_ATENDIMENTO.bytebank.Util;
+
+public class ListaDeContasCorrentes
+{
+    private ContaCorrente[] _itens = null;
+    private int _proximaPosicao = 0;
+
+    public ListaDeContasCorrentes(int tamanhoInicial = 5)
+    {
+        _itens = new ContaCorrente[tamanhoInicial];
+    }
+
+    public void Adicionar(ContaCorrente item)
+    {
+        VerificarCapacidade(_proximaPosicao + 1);
+        Console.WriteLine($"Adicionando item na posição {_proximaPosicao}");
+        _itens[_proximaPosicao] = item;
+        _proximaPosicao++;
+    }
+
+    private void VerificarCapacidade(int tamanhoNecessario)
+    {
+        if(_itens.Length >= tamanhoNecessario)
+        {
+            return;
+        }
+        Console.WriteLine("Aumentando tamanho necessário");
+        ContaCorrente[] novoArray = new ContaCorrente[tamanhoNecessario];
+
+        for (int i = 0; i < _itens.Length; i++)
+        {
+            novoArray[i] = _itens[i];
+        }
+
+        _itens = novoArray;
+    }
+}
